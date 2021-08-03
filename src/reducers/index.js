beda90bdfed7233194e90981986571f18497bcaa
Tomy1123 <rifinyu872@ahk.jp>
2021-08-03 17:33:18 +0900

    create operationLogs reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from "redux";
 
 import events from "./events";
+import opreationLogs from "./operationLogs";
 
-export default combineReducers({ events })
+export default combineReducers({ 
+  events, 
+  opreationLogs 
+})
